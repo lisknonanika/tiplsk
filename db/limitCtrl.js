4a4118302b94a6bcb11aa646920bd9cd572e3120
lisknonanika <Yu-ki@MacBook-Air.local>
2019-04-13 19:31:50 +0900

    add

--- db/limitCtrl.js
+++ db/limitCtrl.js
@@ -7,17 +7,12 @@ module.exports.update = async(target) => {
     try {
         const db = await con.db(config.mongo.db);
         const tbl = await db.collection(config.mongo.collectionLimitCtrl);
-
-        // 指定時間経過したものは削除
         await tbl.deleteMany({name: target, execDate:{$lte: utils.getDateTime(config.twitter[target].interval * -1)}});
-        
-        // 残数が残っていなければ終了
         const data = await tbl.find({name: target}).toArray();
-        if (config.twitter[target].max - data.length <= 0) return {result: false};
-
-        // 登録
+        const remain = config.twitter[target].max - data.length;
+        if (remain <= 0) return {result: true, remain: 0};
         await tbl.insertOne({name: target, execDate: utils.getDateTime()});
-        return {result: true};
+        return {result: true, remain: remain + 1};
     } finally {
         con.close();
     }
---
+++ twitter/dm.js
@@ -0,0 +1,13 @@
+const limitCtrlCollection = require('../db/limitCtrl');
+const config = require('../config');
+
+module.exports = async(twitterId, text) => {
+    const data = await limitCtrlCollection.update(config.twitter.dm.name);
+    if (!data.result || data.remain <= 0) return;
+
+    // DM 送信
+    const params = {event: {type: "message_create",
+                            message_create: {target: {recipient_id: twitterId},
+                                             message_data: {text: text}}}};
+    await config.TwitterClient.post('direct_messages/events/new', params);
+}
